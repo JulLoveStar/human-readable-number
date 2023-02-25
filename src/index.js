992e319ad905ff665d91e1bb6b9919db0390b2b6
Macbook <jul.love.star@gmail.com>
2023-02-25 19:45:40 +0530

    all tests

--- src/index.js
+++ src/index.js
@@ -16,11 +16,15 @@ module.exports = function toReadable(number) {
         result=teen[number[1]]
         // console.log(result)
         return result
-    } else if(number.length === 2 && Number(number[0])>=2){
+    } else if(number.length === 2 && Number(number[0])>=2 && Number(number[1])!=0){
         result = dozen[number[0]] +" "+ unit[number[1]]
         // console.log(result)
         return result
-    } else if(number.length === 3 && Number(number[1])>1){
+    } else if(number.length === 2 && Number(number[0])>=2 && Number(number[1])===0){
+        result = dozen[number[0]]
+        // console.log(result)
+        return result
+    }else if(number.length === 3 && Number(number[1])>1 && Number(number[2])!=0){
         result = unit[number[0]]+' hundred '+dozen[number[1]] +" "+ unit[number[2]]
         // console.log(result)
         return result
@@ -28,10 +32,17 @@ module.exports = function toReadable(number) {
         result = unit[number[0]]+' hundred '+teen[number[2]]
         // console.log(result)
         return result
-    } else if(number.length === 3 && Number(number[1])===0){
+    } else if(number.length === 3 && Number(number[1])===0 && Number(number[2])>0){
         result = unit[number[0]]+' hundred '+ unit[number[2]]
         return result
-    }
+    } else if(number.length === 3 && Number(number[1])>1 && Number(number[2])===0){
+        result = unit[number[0]]+' hundred '+dozen[number[1]]
+        // console.log(result)
+        return result
+    }else if(number.length === 3 && Number(number[1])===0 && Number(number[2])===0){
+        result = unit[number[0]]+' hundred'
+        // console.log(result)
+        return result}
 
 }
 // toReadable (105)
